8486a9a55f2b65488badc967d1e9bcb3d5c21e40
Gareth George <garethgeorge97@gmail.com>
2020-03-01 14:24:53 -0800

    viva la vida

--- Assets/User Interface/ColorChanger.cs
+++ Assets/User Interface/ColorChanger.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class ColorChanger : MonoBehaviour
 {
-    public GameObject gameObject;
-    public GameObject Pompeii;
-    Renderer rend;
-    void Start()
-    {
-        rend = gameObject.GetComponent<Renderer>();
+  public GameObject gameObject;
+  public GameObject Pompeii;
+  Renderer rend;
+  void Start()
+  {
+    rend = gameObject.GetComponent<Renderer>();
 
-    }
+  }
 
-    public void colorChangeGreen()
-    {
-        // rend.material.color = Color.green;
-        Pompeii.SetActive(true);
-    }
+  public void colorChangeGreen()
+  {
+    // rend.material.color = Color.green;
+    Pompeii.SetActive(!Pompeii.activeSelf);
+  }
 
-    public void colorChangeBlue()
-    {
-        rend.material.color = Color.blue;
-    }
+  public void colorChangeBlue()
+  {
+    rend.material.color = Color.blue;
+  }
 
-    public void colorChangeRed()
-    {
-        rend.material.color = Color.red;
-    }
+  public void colorChangeRed()
+  {
+    rend.material.color = Color.red;
+  }
 }
