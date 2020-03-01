4a4aa86ad88445f3c83ced14d448da4983d8520b
Gareth George <garethgeorge97@gmail.com>
2020-03-01 14:17:27 -0800

    implemented proper scoring

--- Assets/Scripts/Audience.cs
+++ Assets/Scripts/Audience.cs
@@ -2,33 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Audience : MonoBehaviour
 {
+  public MicrophoneManager microphoneManager;
 
-    private string[] names = { "idle", "applause", "applause2", "celebration", "celebration2", "celebration3" };
+  // private string[] names = { "idle", "applause", "applause2", "celebration", "celebration2", "celebration3" };
+  private string[] animationNames = { "idle", "applause", "applause2", "idle", "applause", "applause2", "celebration" };
 
-    // Use this for initialization
-    void Start()
+  // Use this for initialization
+  void Start()
+  {
+    Animation[] AudienceMembers = gameObject.GetComponentsInChildren<Animation>();
+    foreach (Animation anim in AudienceMembers)
     {
-        Animation[] AudienceMembers = gameObject.GetComponentsInChildren<Animation>();
-        foreach (Animation anim in AudienceMembers)
-        {
-            StartCoroutine(setAnimation(anim));
-        }
+      StartCoroutine(setAnimation(anim));
+    }
+  }
+
+  IEnumerator setAnimation(Animation anim)
+  {
+    string thisAnimation = "idle";
+    if (MicrophoneManager.isMatching)
+    {
+      thisAnimation = animationNames[Random.Range(0, animationNames.Length)];
     }
 
-    IEnumerator setAnimation(Animation anim)
-    {
-        string thisAnimation = names[Random.Range(0, 5)];
-        float timePeriod = Random.Range(0f, 4f);
+    float timePeriod = Random.Range(0f, 4f);
 
-        anim.wrapMode = WrapMode.Loop;
-        anim.GetComponent<Animation>().CrossFade(thisAnimation);
-        anim[thisAnimation].time = timePeriod;
+    anim.wrapMode = WrapMode.Loop;
+    anim.GetComponent<Animation>().CrossFade(thisAnimation);
+    anim[thisAnimation].time = timePeriod;
 
-        yield return new WaitForSeconds(timePeriod);
-        StartCoroutine(setAnimation(anim));
-        yield return null;
-    }
+    yield return new WaitForSeconds(timePeriod);
+    StartCoroutine(setAnimation(anim));
+    yield return null;
+  }
 }
--- Assets/Scripts/Microphone/MicrophoneManager.cs
+++ Assets/Scripts/Microphone/MicrophoneManager.cs
@@ -11,7 +11,9 @@ public class MicrophoneManager : MonoBehaviour
 
   private PitchTracker audioInTracker;
   private PitchTracker audioReferenceTracker;
+  private int audioRefPitchLastUpdate = 0;
   private PitchTracker.PitchRecord audioRefPitch;
+  private int audioInPitchLastUpdate = 0;
   private PitchTracker.PitchRecord audioInPitch;
 
   public int bufferSize = 1024;
@@ -22,7 +24,8 @@ public class MicrophoneManager : MonoBehaviour
   private int recentRefPitchesIdx = 1000; // dumb fix with some index out of bounds errors 
   public int[] recentRefPitches;
 
-  public bool isMatching = false;
+  public static bool isMatching = false;
+  public static float score = 0;
 
   void Start()
   {
@@ -50,10 +53,13 @@ public class MicrophoneManager : MonoBehaviour
 
     buffer = new float[bufferSize];
     recentRefPitches = new int[60];
+    score = 0;
   }
 
   bool FindMatch(int offset, int windowLength, int sampleNote, int forgiveness = 1)
   {
+    if (sampleNote == 0)
+      return false;
     for (int i = -offset - windowLength; i < -offset; ++i)
     {
       int value = recentRefPitches[(recentRefPitchesIdx + i) % recentRefPitches.Length];
@@ -73,22 +79,33 @@ public class MicrophoneManager : MonoBehaviour
     audioReferenceTracker.ProcessBuffer(buffer);
 
     PitchTracker.PitchRecord curPitch = audioInTracker.CurrentPitchRecord;
+    audioInPitchLastUpdate++;
     PitchTracker.PitchRecord curPitchRef = audioReferenceTracker.CurrentPitchRecord;
-    if (curPitch.MidiNote != 0)
+    audioRefPitchLastUpdate++;
+    if (curPitch.MidiNote != 0 || audioInPitchLastUpdate > 60)
+    {
       audioInPitch = curPitch;
-    if (curPitchRef.MidiNote != 0)
+      audioInPitchLastUpdate = 0;
+    }
+    if (curPitchRef.MidiNote != 0 || audioRefPitchLastUpdate > 60)
+    {
       audioRefPitch = curPitchRef;
+      audioRefPitchLastUpdate = 0;
+    }
     recentRefPitches[(recentRefPitchesIdx++) % recentRefPitches.Length] = audioRefPitch.MidiNote;
 
-    string text = "Current note is: " + audioInPitch.MidiNote + "\nExpected: " + audioRefPitch.MidiNote;
+    string text = "Current pitch: " + audioInPitch.MidiNote + "\nTarget: " + audioRefPitch.MidiNote;
 
     if (this.FindMatch(0, recentRefPitches.Length, audioInPitch.MidiNote))
     {
       isMatching = true;
-      text = "IT IS A MATCH!!! WOOHOO!!!";
+      text = "MATCHING! GREAT JOB!!!";
+      score += Time.deltaTime;
     }
     else
       isMatching = false;
+
+    text += "\nScore: " + Mathf.Round(score * 1000).ToString();
 
     message.text = text;
   }
