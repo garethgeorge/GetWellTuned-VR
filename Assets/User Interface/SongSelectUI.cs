ef2c7244f00de28aaba831f2d57d82d66117b39a
petergartland <petergartland@ucsb.edu>
2020-03-03 14:40:08 -0800

    UI has been fixed. Restart button works

---
+++ Assets/User Interface/SongSelectUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongSelectUI : MonoBehaviour
+{
+  public GameObject Pompeii;
+  public GameObject ActiveSong;
+  void Start()
+  {
+
+  }
+
+  public void playPompeii()
+  {
+    Pompeii.SetActive(true);
+    ActiveSong.SetActive(true);
+  }
+
+}
