c4e260d31d01f032e9f091157d79147e938cb341
Dana <dana.nguyensths@gmail.com>
2020-03-03 10:03:27 -0800

    adding audience control code

---
+++ Assets/CrowdManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdManager : MonoBehaviour
+{
+    public GameObject crowd0;
+    public GameObject crowd10;
+    // Start is called before the first frame update
+
+
+
+}
--- Assets/User Interface/ColorChanger.cs
+++ Assets/User Interface/ColorChanger.cs
@@ -4,34 +4,62 @@ using UnityEngine;
 
 public class ColorChanger : MonoBehaviour
 {
-  public GameObject gameObject;
-  public GameObject Pompeii;
-  public GameObject ActiveSong;
-  Renderer rend;
- // UnityEngine.Video.VideoPlayer screen;
-  void Start()
-  {
-    rend = gameObject.GetComponent<Renderer>();
-  //  screen = Pompeii.GetComponent<UnityEngine.Video.VideoPlayer>();
+    public GameObject gameObject;
+    public GameObject Pompeii;
+    public GameObject ActiveSong;
+    Renderer rend;
+
+    public GameObject Audience;
+    public int crowd_size;
+    public List<Animation> audienceActive;
+    // UnityEngine.Video.VideoPlayer screen;
+    void Start()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        //  screen = Pompeii.GetComponent<UnityEngine.Video.VideoPlayer>();
 
 
-  }
+    }
 
-  public void colorChangeGreen()
-  {
-    // rend.material.color = Color.green;
-    Pompeii.SetActive(true);
-    ActiveSong.SetActive(true);
-  //  screen.SetActive(true);
-  }
+    public void colorChangeGreen()
+    {
+        // rend.material.color = Color.green;
+        Pompeii.SetActive(true);
+        ActiveSong.SetActive(true);
+        //  screen.SetActive(true);
+    }
 
-  public void colorChangeBlue()
-  {
-    rend.material.color = Color.blue;
-  }
+    public void colorChangeBlue()
+    {
+        rend.material.color = Color.blue;
+    }
 
-  public void colorChangeRed()
-  {
-    rend.material.color = Color.red;
-  }
+    public void colorChangeRed()
+    {
+        rend.material.color = Color.red;
+    }
+
+    public void audienceActivate0()
+    {
+
+        Audience.SetActive(true);
+        crowd_size = 0;
+
+
+    }
+
+    public void audienceActivate10()
+    {
+
+        Audience.SetActive(true);
+        crowd_size = 10;
+
+        Animation[] AudienceMembers = Audience.GetComponentsInChildren<Animation>();
+
+        // depends on which button clicked
+        for (int i = 0; i < crowd_size; i++)
+        {
+            audienceActive.Add(AudienceMembers[Random.Range(0, AudienceMembers.Length)]);
+        }
+    }
 }
