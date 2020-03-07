c46ecc257bbd2dc59421ae978d8bcf547ae11acb
Gareth George <garethgeorge97@gmail.com>
2020-03-06 21:27:16 -0800

    disco

---
+++ Assets/Scripts/DiscoTileController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoTileController : MonoBehaviour
+{
+  private GameObject discoTile;
+  private static Color[] colorOptions;
+
+  private Color myColor;
+  private Color myColorDim;
+
+  private Material myMat;
+  private Material myMatDim;
+  private MeshRenderer myRenderer;
+
+  void Start()
+  {
+    if (colorOptions == null)
+    {
+      colorOptions = new Color[7]{
+        new Color(1f, 0f, 0f),
+        new Color(0f, 1f, 0f),
+        new Color(0f, 0f, 1f),
+        new Color(0f, 1f, 1f),
+        new Color(1f, 1f, 0f),
+        new Color(1f, 0f, 1f),
+        new Color(1f, 0.5f, 1f),
+      };
+    }
+
+    myColor = colorOptions[Random.Range(0, colorOptions.Length)];
+    myColorDim = Color.Lerp(myColor, Color.black, 0.7f);
+
+    myMat = new Material(Shader.Find("Glow"));
+    myMatDim = new Material(Shader.Find("Standard"));
+    myMat.color = myColor;
+    myMatDim.color = myColorDim;
+
+    myRenderer = (MeshRenderer)GetComponentInParent<MeshRenderer>();
+    myRenderer.material = myMatDim;
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    myRenderer.material = myMat;
+  }
+
+  IEnumerator OnTriggerExit(Collider other)
+  {
+    yield return new WaitForSeconds(1);
+    myRenderer.material = myMatDim;
+    yield return null;
+  }
+}
--- Assets/Scripts/Microphone/CanvasPlotPitch.cs
+++ Assets/Scripts/Microphone/CanvasPlotPitch.cs
@@ -15,8 +15,8 @@ public class CanvasPlotPitch : MonoBehaviour
   public RawImage rawImage;
   void Start()
   {
-    width = 64;
-    height = 12;
+    width = 128;
+    height = 24;
     drawTexture = new Texture2D(width, height);
     fillRect(drawTexture, 0, 0, width, height, Color.green);
     rawImage.texture = drawTexture;
@@ -30,53 +30,6 @@ public class CanvasPlotPitch : MonoBehaviour
       array[idx] = color;
     }
     texture.SetPixels(x, y, w, h, array);
-  }
-
-  void drawLine(Texture2D tex, int x0, int y0, int x1, int y1, Color col)
-  {
-    var dy = y1 - y0;
-    var dx = x1 - x0;
-
-    int stepy;
-    int stepx;
-    if (dy < 0) { dy = -dy; stepy = -1; }
-    else { stepy = 1; }
-    if (dx < 0) { dx = -dx; stepx = -1; }
-    else { stepx = 1; }
-    dy <<= 1;
-    dx <<= 1;
-
-    tex.SetPixel(x0, y0, col);
-    if (dx > dy)
-    {
-      var fraction = dy - (dx >> 1);
-      while (x0 != x1)
-      {
-        if (fraction >= 0)
-        {
-          y0 += stepy;
-          fraction -= dx;
-        }
-        x0 += stepx;
-        fraction += dy;
-        tex.SetPixel(x0, y0, col);
-      }
-    }
-    else
-    {
-      var fraction = dx - (dy >> 1);
-      while (y0 != y1)
-      {
-        if (fraction >= 0)
-        {
-          x0 += stepx;
-          fraction -= dy;
-        }
-        y0 += stepy;
-        fraction += dx;
-        tex.SetPixel(x0, y0, col);
-      }
-    }
   }
 
   void Update()
