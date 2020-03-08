2d805f630c195c6e00c4135d55321afa448b9607
Dana <dana.nguyensths@gmail.com>
2020-03-07 17:41:35 -0800

    audience controller

--- Assets/User Interface/SongSelectUI.cs
+++ Assets/User Interface/SongSelectUI.cs
@@ -4,17 +4,69 @@ using UnityEngine;
 
 public class SongSelectUI : MonoBehaviour
 {
-  public GameObject Pompeii;
-  public GameObject ActiveSong;
-  void Start()
-  {
+    public GameObject Pompeii;
+    public GameObject ActiveSong;
+    public GameObject Crowd;
+    private int crowdSize;
+    void Start()
+    {
 
-  }
+    }
 
-  public void playPompeii()
-  {
-    Pompeii.SetActive(true);
-    ActiveSong.SetActive(true);
-  }
+    public void playPompeii()
+    {
+        Pompeii.SetActive(true);
+        ActiveSong.SetActive(true);
+    }
+
+    public void activateCrowd_0()
+    {
+        foreach (Transform child in Crowd.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+    }
+    public void activateCrowd_3()
+    {
+        crowdSize = 3;
+
+        foreach (Transform child in Crowd.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+
+        for (int i = 0; i < crowdSize; i++)
+        {
+
+            Crowd.transform.GetChild(i).gameObject.SetActive(true);
+        }
+
+    }
+
+    public void activateCrowd_6()
+    {
+        crowdSize = 6;
+
+        foreach (Transform child in Crowd.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        foreach (Transform child in Crowd.transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+        // for (int i = 0; i < crowdSize; i++)
+        // {
+        //     Crowd.transform.GetChild(i).gameObject.SetActive(true);
+        // }
+
+    }
+
+
+
+
 
 }
